19060fc8a107ce0e0d01f1ef1eb779ef579e70fa
isaadsalman7 <39169341+isaadsalman7@users.noreply.github.com>
2023-09-25 14:49:40 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 3460327766185
+let accountEmail = "i.saadsalman7@gmail.com"
+var accountPassword = "sr4oficial"
+accountCity = "Sialkot"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "mysocialhandlers@gmail.com"
+accountPassword = "Rgh7SrwwQQ"
+accountCity = "Lahore"
+
+// console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
