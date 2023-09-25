6cf3ae35c0a017edf65db1fc09f36eb9232efcf6
isaadsalman7 <39169341+isaadsalman7@users.noreply.github.com>
2023-09-25 15:21:07 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+//"use strict"; //treat all JS as new JS
+
+// alert(3+3) We are using NodeJS, not browser
+
+console.log(3+3)
+console.log("Saad")
+
+let name = "Saad"
+let age = 26
+let isLoggedIn = false
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol 
