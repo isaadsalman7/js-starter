51391f36e8f02541c76fdfef80f00bc0248b1509
Saad Salman <39169341+isaadsalman7@users.noreply.github.com>
2023-09-26 13:57:07 +0000

    Datatype Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// PRIMITIVE DATATYPES
+// 7 TYPES
+// CALL BY VALUE STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, SYMBOL, BIGINT
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id === anotherID);
+
+const bigNumber = 544482123465445432121321n
+
+
+// REFERENCE NON-PRIMITIVE DATATYPES MEMORY MAI REFERENCE DIRECTLY ALLOCATE
+// ARRAY, OBJECTS
+
+
+const heros = ["Spiderman", "Batman", "Superman"]
+
+
+let myObj =  
+
+{
+    name: "Saad",
+    age: 26,
+    
+}
+
+const myFunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherID);
+
+// TO MASTER JS, MASTER OBJECTS and WEB EVENTS
