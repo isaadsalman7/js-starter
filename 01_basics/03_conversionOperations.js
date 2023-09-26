c05aaca002ea10b6f6a358c648d7c5258bace52f
Saad Salman <39169341+isaadsalman7@users.noreply.github.com>
2023-09-26 11:22:06 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "Saad"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+let isLoggedIn = "Saad"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber);
