8f1346530b9676b29ff504a5fe3080e347bf8c16
Saad Salman <39169341+isaadsalman7@users.noreply.github.com>
2023-09-26 12:06:27 +0000

    Conversion Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "Saad"
+/* let score = "Saad"
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -17,4 +17,37 @@ let someNumber = 33
 
 let StringNumber = String(someNumber)
 console.log(StringNumber)
-console.log(typeof StringNumber);
+console.log(typeof StringNumber);*/
+
+///////////OPERATIONS/////////////
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "Saad"
+let str3 = str1 + str2 
+
+// console.log(str3);
+
+// console.log("1" + 2 + 2)
+// console.log("1" + 2)
+// console.log(1 + "2")
+//  console.log(1+(2+"2"))
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 4
+
+// console.log(num1, num2, num3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
