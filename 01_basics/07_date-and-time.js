dd234dfbf2dfef0475cb99ad4defcbed0505783b
Saad Salman <39169341+isaadsalman7@users.noreply.github.com>
2023-09-27 10:07:11 +0000

    Date and Time

---
+++ 01_basics/07_date-and-time.js
@@ -0,0 +1,30 @@
+let myDate = new Date ()
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23,5,3,9)
+// let myCreatedDate = new Date(2023, 0, 23,5,3,9)
+let myCreatedDate = new Date("1996-01-28")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+// newDate.toLocaleString('default', (
+
+//     weekday: "long"
+//     ))
