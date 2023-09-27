517cc6e1cebebf74c1b5453028d7c2d895e22a3e
Saad Salman <39169341+isaadsalman7@users.noreply.github.com>
2023-09-27 11:45:22 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHero = ["Spiderman", "batman", "superman"]
+
+// console.log(myArr[4]);
+// console.log(myHero[1]);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.unshift(9)
+
+// console.log(myArr);
+// myArr.shift()
+
+// myArr.pop()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(typeof myArr);
+// console.log(typeof newArr);
+
+// slice , splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+// console.log("C", myArr);
+console.log("C", myArr);
+console.log(myn2);
